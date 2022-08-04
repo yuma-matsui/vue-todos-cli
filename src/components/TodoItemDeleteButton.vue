5ab61c3c813d166e09014224551c0c9fbab62189
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-04 20:07:21 +0900

    TodoItem削除用のボタンコンポーネント

---
+++ src/components/TodoItemDeleteButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="deleteItem">削除</button>
+</template>
+
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+
+  emits: ['delete-item'],
+
+  methods: {
+    deleteItem () {
+      this.$emit('delete-item', this.index)
+    }
+  }
+}
+</script>
