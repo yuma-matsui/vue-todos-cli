bd50c802a9d869b8e1698505100802cd11b6114d
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-04 20:07:58 +0900

    TodoItem編集ボタンコンポーネント

---
+++ src/components/TodoItemEditButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="editItem">編集</button>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+
+  emits: ['edit-item'],
+
+  methods: {
+    editItem () {
+      this.$emit('edit-item', this.item)
+    }
+  }
+}
+</script>
