40290989298be1f3b5cf6fc5f6825ab7ddba4ac4
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-06 09:25:48 +0900

    router-view && router-linkの利用

---
+++ src/views/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <router-view />
+    <div>
+      <router-link to="/">Home |</router-link>
+      <router-link to="/todos">TodoApp |</router-link>
+    </div>
+  </div>
+</template>
---
+++ src/views/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>{{ item.title }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    index: {
+      type: String,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      todoItems: []
+    }
+  },
+
+  created () {
+    const todoItems = JSON.parse(localStorage.getItem('todoItems'))
+    this.todoItems = todoItems || []
+  },
+
+  computed: {
+    indexNum () {
+      return Number(this.index)
+    },
+
+    item () {
+      const index = Number(this.index)
+      return this.todoItems[index]
+    }
+  }
+}
+</script>
